49f571b41c06ec490295cdb042886fbd508974e5
ye111111ow@gmail.com <ye111111ow@gmail.com@c2dd1ff9-a1ee-2fcc-325c-97cee1f8507e>
2011-11-09 03:59:26 +0000

    v2.0.0

--- trunk/WeibokongNew/Chrome/settings.js
+++ trunk/WeibokongNew/Chrome/settings.js
@@ -1,93 +1,88 @@
 	window.addEvent("domready", function () {
     new FancySettings.initWithManifest(function (settings) {
 	
-		document.getElementById("settings-label").innerHTML = "Œ¢≤©øÿ…Ë÷√";
+		document.getElementById("settings-label").innerHTML = "Œ¢≤©–¬øÿ…Ë÷√";
 		document.getElementById("search").placeholder = "À—À˜…Ë÷√—°œÓ...";
 		document.getElementById("search-label").innerHTML = "À—À˜";
+		/*
+			Top
+		*/
 		var me = settings.manifest.hide_top.label;
 		me.addEventListener("mouseover", function(){hover("hide_top");}, false);
+		me.addEventListener("mouseout", unhover, false);
+		
+		me = settings.manifest.hide_top_logo.label;
+		me.addEventListener("mouseover", function(){hover("hide_top_logo");}, false);
 		me.addEventListener("mouseout", unhover, false);
 		
 		me = settings.manifest.hide_top_left.label;
 		me.addEventListener("mouseover", function(){hover("hide_top_left");}, false);
 		me.addEventListener("mouseout", unhover, false);
 		
+		me = settings.manifest.hide_top_search.label;
+		me.addEventListener("mouseover", function(){hover("hide_top_search");}, false);
+		me.addEventListener("mouseout", unhover, false);
+		
 		me = settings.manifest.hide_top_right.label;
 		me.addEventListener("mouseover", function(){hover("hide_top_right");}, false);
 		me.addEventListener("mouseout", unhover, false);
 		
-		me = settings.manifest.hide_top_tip.label;
-		me.addEventListener("mouseover", function(){hover("hide_top_tip");}, false);
-		me.addEventListener("mouseout", unhover, false);
 		
-		me = settings.manifest.hide_secondnav.label;
-		me.addEventListener("mouseover", function(){hover("hide_secondnav");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_secondnav_logo.label;
-		me.addEventListener("mouseover", function(){hover("hide_secondnav_logo");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_secondnav_nav.label;
-		me.addEventListener("mouseover", function(){hover("hide_secondnav_nav");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_right.label;
-		me.addEventListener("mouseover", function(){hover("hide_right");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_right_widgets.label;
-		me.addEventListener("mouseover", function(){hover("hide_right_widgets");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
+		/*
+			Right
+		*/
 		me = settings.manifest.hide_right_ads.label;
 		me.addEventListener("mouseover", function(){hover("hide_right_ads");}, false);
 		me.addEventListener("mouseout", unhover, false);
 		
-		me = settings.manifest.hide_right_tip.label;
-		me.addEventListener("mouseover", function(){hover("hide_right_tip");}, false);
+		me = settings.manifest.hide_right_tips.label;
+		me.addEventListener("mouseover", function(){hover("hide_right_tips");}, false);
 		me.addEventListener("mouseout", unhover, false);
 		
 		me = settings.manifest.hide_right_info.label;
 		me.addEventListener("mouseover", function(){hover("hide_right_info");}, false);
 		me.addEventListener("mouseout", unhover, false);
 		
-		me = settings.manifest.hide_right_app.label;
-		me.addEventListener("mouseover", function(){hover("hide_right_app");}, false);
+		me = settings.manifest.hide_right_medals.label;
+		me.addEventListener("mouseover", function(){hover("hide_right_medals");}, false);
 		me.addEventListener("mouseout", unhover, false);
 		
 		me = settings.manifest.hide_right_nav.label;
 		me.addEventListener("mouseover", function(){hover("hide_right_nav");}, false);
 		me.addEventListener("mouseout", unhover, false);
 		
-		me = settings.manifest.hide_right_cf.label;
-		me.addEventListener("mouseover", function(){hover("hide_right_cf");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_right_report.label;
-		me.addEventListener("mouseover", function(){hover("hide_right_report");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
 		me = settings.manifest.hide_right_topic.label;
 		me.addEventListener("mouseover", function(){hover("hide_right_topic");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_right_people.label;
-		me.addEventListener("mouseover", function(){hover("hide_right_people");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_right_alltopic.label;
-		me.addEventListener("mouseover", function(){hover("hide_right_alltopic");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_right_group.label;
-		me.addEventListener("mouseover", function(){hover("hide_right_group");}, false);
 		me.addEventListener("mouseout", unhover, false);
 		
 		me = settings.manifest.hide_right_popuser.label;
 		me.addEventListener("mouseover", function(){hover("hide_right_popuser");}, false);
 		me.addEventListener("mouseout", unhover, false);
 		
+		me = settings.manifest.hide_right_allinone.label;
+		me.addEventListener("mouseover", function(){hover("hide_right_allinone");}, false);
+		me.addEventListener("mouseout", unhover, false);
+		
+		me = settings.manifest.hide_right_alltopic.label;
+		me.addEventListener("mouseover", function(){hover("hide_right_alltopic");}, false);
+		me.addEventListener("mouseout", unhover, false);
+		
+		me = settings.manifest.hide_right_notice.label;
+		me.addEventListener("mouseover", function(){hover("hide_right_notice");}, false);
+		me.addEventListener("mouseout", unhover, false);
+		
+		me = settings.manifest.hide_right_help.label;
+		me.addEventListener("mouseover", function(){hover("hide_right_help");}, false);
+		me.addEventListener("mouseout", unhover, false);
+		
+		me = settings.manifest.hide_right_feedback.label;
+		me.addEventListener("mouseover", function(){hover("hide_right_feedback");}, false);
+		me.addEventListener("mouseout", unhover, false);
+		
+		
+		/*
+			Main
+		*/
 		me = settings.manifest.hide_main_post.label;
 		me.addEventListener("mouseover", function(){hover("hide_main_post");}, false);
 		me.addEventListener("mouseout", unhover, false);
@@ -96,26 +91,18 @@
 		me.addEventListener("mouseover", function(){hover("hide_main_tips");}, false);
 		me.addEventListener("mouseout", unhover, false);
 		
-		me = settings.manifest.hide_main_sort.label;
-		me.addEventListener("mouseover", function(){hover("hide_main_sort");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_main_forward.label;
-		me.addEventListener("mouseover", function(){hover("hide_main_forward");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_main_image.label;
-		me.addEventListener("mouseover", function(){hover("hide_main_image");}, false);
+		me = settings.manifest.hide_main_nav.label;
+		me.addEventListener("mouseover", function(){hover("hide_main_nav");}, false);
 		me.addEventListener("mouseout", unhover, false);
 		
 		me = settings.manifest.hide_main_micro.label;
 		me.addEventListener("mouseover", function(){hover("hide_main_micro");}, false);
 		me.addEventListener("mouseout", unhover, false);
 		
-		me = settings.manifest.hide_other_nav.label;
-		me.addEventListener("mouseover", function(){hover("hide_other_nav");}, false);
-		me.addEventListener("mouseout", unhover, false);
 		
+		/*
+			Other
+		*/
 		me = settings.manifest.hide_other_ads.label;
 		me.addEventListener("mouseover", function(){hover("hide_other_ads");}, false);
 		me.addEventListener("mouseout", unhover, false);
@@ -124,42 +111,36 @@
 		me.addEventListener("mouseover", function(){hover("hide_other_bot");}, false);
 		me.addEventListener("mouseout", unhover, false);
 		
-		me = settings.manifest.hide_top_friend.label;
+		
+		/*
+			Friend
+		*/
+		/*
+			Top
+		*/
+		var me = settings.manifest.hide_top_friend.label;
 		me.addEventListener("mouseover", function(){hover("hide_top");}, false);
+		me.addEventListener("mouseout", unhover, false);
+		
+		me = settings.manifest.hide_top_logo_friend.label;
+		me.addEventListener("mouseover", function(){hover("hide_top_logo");}, false);
 		me.addEventListener("mouseout", unhover, false);
 		
 		me = settings.manifest.hide_top_left_friend.label;
 		me.addEventListener("mouseover", function(){hover("hide_top_left");}, false);
 		me.addEventListener("mouseout", unhover, false);
 		
+		me = settings.manifest.hide_top_search_friend.label;
+		me.addEventListener("mouseover", function(){hover("hide_top_search");}, false);
+		me.addEventListener("mouseout", unhover, false);
+		
 		me = settings.manifest.hide_top_right_friend.label;
 		me.addEventListener("mouseover", function(){hover("hide_top_right");}, false);
 		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_secondnav_friend.label;
-		me.addEventListener("mouseover", function(){hover("hide_secondnav");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_secondnav_logo_friend.label;
-		me.addEventListener("mouseover", function(){hover("hide_secondnav_logo");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_secondnav_nav_friend.label;
-		me.addEventListener("mouseover", function(){hover("hide_secondnav_nav");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_other_nav_friend.label;
-		me.addEventListener("mouseover", function(){hover("hide_other_nav");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_other_ads_friend.label;
-		me.addEventListener("mouseover", function(){hover("hide_other_ads");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_other_bot_friend.label;
-		me.addEventListener("mouseover", function(){hover("hide_other_bot");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
+
+		/*
+			Right
+		*/
 		me = settings.manifest.hide_right_info_friend.label;
 		me.addEventListener("mouseover", function(){hover("hide_right_info_friend");}, false);
 		me.addEventListener("mouseout", unhover, false);
@@ -176,10 +157,6 @@
 		me.addEventListener("mouseover", function(){hover("hide_right_bothfollow_friend");}, false);
 		me.addEventListener("mouseout", unhover, false);
 		
-		me = settings.manifest.hide_right_profile_friend.label;
-		me.addEventListener("mouseover", function(){hover("hide_right_profile_friend");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
 		me = settings.manifest.hide_right_tag_friend.label;
 		me.addEventListener("mouseover", function(){hover("hide_right_tag_friend");}, false);
 		me.addEventListener("mouseout", unhover, false);
@@ -192,10 +169,6 @@
 		me.addEventListener("mouseover", function(){hover("hide_right_fan_friend");}, false);
 		me.addEventListener("mouseout", unhover, false);
 		
-		me = settings.manifest.hide_right_group_friend.label;
-		me.addEventListener("mouseover", function(){hover("hide_right_group_friend");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
 		me = settings.manifest.hide_right_feedback_friend.label;
 		me.addEventListener("mouseover", function(){hover("hide_right_feedback_friend");}, false);
 		me.addEventListener("mouseout", unhover, false);
@@ -204,12 +177,19 @@
 		me.addEventListener("mouseover", function(){hover("hide_right_report_friend");}, false);
 		me.addEventListener("mouseout", unhover, false);
 		
+		/*
+			Main
+		*/
 		me = settings.manifest.hide_main_relation_friend.label;
 		me.addEventListener("mouseover", function(){hover("hide_main_relation_friend");}, false);
 		me.addEventListener("mouseout", unhover, false);
 		
 		me = settings.manifest.hide_main_handle_friend.label;
 		me.addEventListener("mouseover", function(){hover("hide_main_handle_friend");}, false);
+		me.addEventListener("mouseout", unhover, false);
+		
+		me = settings.manifest.hide_main_album_friend.label;
+		me.addEventListener("mouseover", function(){hover("hide_main_album_friend");}, false);
 		me.addEventListener("mouseout", unhover, false);
 		
 		me = settings.manifest.hide_main_tab_friend.label;
@@ -223,57 +203,12 @@
 		me = settings.manifest.hide_main_micro_friend.label;
 		me.addEventListener("mouseover", function(){hover("hide_main_micro");}, false);
 		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_top_search.label;
-		me.addEventListener("mouseover", function(){hover("hide_top");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_top_left_search.label;
-		me.addEventListener("mouseover", function(){hover("hide_top_left");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_top_right_search.label;
-		me.addEventListener("mouseover", function(){hover("hide_top_right");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_search_logo.label;
-		me.addEventListener("mouseover", function(){hover("hide_secondnav_logo");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_search_bottom.label;
+	
+		/*
+			Other
+		*/
+		me = settings.manifest.hide_other_bot_friend.label;
 		me.addEventListener("mouseover", function(){hover("hide_other_bot");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_search_hot.label;
-		me.addEventListener("mouseover", function(){hover("hide_search_hot");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_search_order.label;
-		me.addEventListener("mouseover", function(){hover("hide_search_order");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_search_topic.label;
-		me.addEventListener("mouseover", function(){hover("hide_search_topic");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_search_toptitle.label;
-		me.addEventListener("mouseover", function(){hover("hide_search_toptitle");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_search_top10.label;
-		me.addEventListener("mouseover", function(){hover("hide_search_top10");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_search_related.label;
-		me.addEventListener("mouseover", function(){hover("hide_search_related");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.hide_search_feedback.label;
-		me.addEventListener("mouseover", function(){hover("hide_search_feedback");}, false);
-		me.addEventListener("mouseout", unhover, false);
-		
-		me = settings.manifest.filter.label;
-		me.addEventListener("mouseover", function(){hover("filter");}, false);
 		me.addEventListener("mouseout", unhover, false);
 
     });
